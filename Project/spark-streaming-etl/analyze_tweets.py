fd7927d484adde10f28bba94d8df086f7c224e0e
Jeroen Hofman <jeroen.hofman@booking.com>
2021-02-04 18:59:20 +0100

    Added first working run of a docker compose with 2 steps (for now), one setting up an FTP server querying the twitter api and sending it to the 2nd part, the spark streaming app which analyzes hashtags. Now that the skeleton is working I will continue on the other parts.

---
+++ Project/spark-streaming-etl/analyze_tweets.py
@@ -0,0 +1,27 @@
+from pyspark import SparkConf,SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark.sql import Row,SQLContext
+import sys
+import requests
+# create spark configuration
+conf = SparkConf()
+conf.setAppName("TwitterStreamApp")
+# create spark context with the above configuration
+sc = SparkContext(conf=conf)
+sc.setLogLevel("ERROR")
+# create the Streaming Context from the above spark context with interval size 2 seconds
+ssc = StreamingContext(sc, 2)
+# setting a checkpoint to allow RDD recovery
+ssc.checkpoint("checkpoint_TwitterApp")
+# read data from port 9009
+dataStream = ssc.socketTextStream("localhost",65431)
+
+# split each tweet into words
+words = dataStream.flatMap(lambda line: line.split(" "))
+# filter the words to get only hashtags, then map each hashtag to be a pair of (hashtag,1)
+hashtags = words.filter(lambda w: '#' in w).map(lambda x: (x, 1))
+
+hashtags.pprint()
+
+ssc.start()
+ssc.awaitTermination()
---
+++ Project/tcp-server/setup_tcp_server.py
@@ -0,0 +1,47 @@
+import socket
+import sys
+import requests
+import requests_oauthlib
+import json
+
+CONSUMER_KEY='6zJrHzT20xlwlc51ZmOZn3kDv'
+CONSUMER_SECRET='aU56p5rXbUIClZJ8WuSCJQfNYLxRGQGFxChVmI2K6xb6NTupiZ'
+ACCESS_TOKEN='1331972930012590081-ulgnrJGFuGhMWKs7hxYGhwS3ozmrvL'
+ACCESS_SECRET='AtpRu5sFKSWb3253miDzkswKv7Uf87Lxrk73HJ7h0Zfti'
+
+my_auth = requests_oauthlib.OAuth1(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET)
+
+def get_tweets():
+    url = 'https://stream.twitter.com/1.1/statuses/filter.json'
+    query_data = [('language', 'en'), ('locations', '-130,-20,100,50'), ('track', '#')]
+    query_url = url + '?' + '&'.join([str(t[0]) + '=' + str(t[1]) for t in query_data])
+    response = requests.get(query_url, auth=my_auth, stream=True)
+    print(query_url, response)
+    return response
+
+def send_tweets_to_spark(http_resp, tcp_connection):
+    for line in http_resp.iter_lines():
+        try:
+            full_tweet = json.loads(line)
+            tweet_text = full_tweet['text']
+            print("Tweet Text: " + tweet_text)
+            print("------------------------------------------")
+            tcp_connection.send((tweet_text + '\n').encode())
+        except:
+            e = sys.exc_info()[0]
+            print("Error: %s" % e)
+
+
+TCP_IP = ""
+TCP_PORT = 65431
+conn = None
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((TCP_IP, TCP_PORT))
+s.listen(1)
+print("Waiting for TCP connection...")
+conn, addr = s.accept()
+
+print("Connected... Starting getting tweets.")
+print('Connected by', addr)
+resp = get_tweets()
+send_tweets_to_spark(resp, conn)
